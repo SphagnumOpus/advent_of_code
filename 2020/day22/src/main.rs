447c8cc976ee400534cea0b51b51711f19acc819
SphagnumOpus <william@mossfamily.net>
2021-01-03 09:24:56 -0500

    Completed Part 1 & Part 2 of Day 22

--- 2020/day22/src/main.rs
+++ 2020/day22/src/main.rs
@@ -1,8 +1,6 @@
 use std::io;
-use std::collections::HashSet;
-use std::collections::HashMap;
-use std::collections::BTreeMap;
 use std::collections::VecDeque;
+use std::option::Option;
 
 //Day 22 2020 for advent of code
 //
@@ -13,12 +11,11 @@ use std::collections::VecDeque;
 struct CardGame {
     player1: VecDeque<Card>,
     player2: VecDeque<Card>,
+    history: Vec<CardGame>
 }
 type Card = u32;
 type Score = u32;
 type FinalAnswer = u32;
-type NumberOfRounds = u32;
-
 
 
 fn main() {
@@ -43,7 +40,8 @@ fn read_input() -> Vec<String> {
 fn build_this_card_game(inp:&Vec<String>) -> CardGame {
     let mut out = CardGame {
         player1: VecDeque::new(),
-        player2: VecDeque::new()
+        player2: VecDeque::new(),
+        history: Vec::new()
     };
     let mut which_player=&mut out.player1;
 
@@ -68,35 +66,112 @@ fn build_this_card_game(inp:&Vec<String>) -> CardGame {
 
 fn calc_part_1(inp:&CardGame) ->FinalAnswer{
     let mut clone = (*inp).clone();
-    let (out,_number_of_rounds) = play_game(&mut clone);
+    let (out,_did_1_win) = play_game(&mut clone,false);
     out
 }
 fn calc_part_2(inp:&CardGame) ->FinalAnswer{
-0
+    let mut clone = (*inp).clone();
+    let (out,_did_1_win) = play_game(&mut clone,true);
+    out
 }
 
-fn play_game(inp:&mut CardGame) -> (Score,NumberOfRounds) {
-    let mut counter=0;
+fn play_game(mut inp:&mut CardGame,inp_is_recursive:bool) -> (Score,bool) {
     while (inp.player1.len()>0) && (inp.player2.len()>0) {
-        let (mut winner,loser) = match inp.player1.front().unwrap() > inp.player2.front().unwrap() {
-            true =>  (&mut inp.player1,&mut inp.player2),
-            false => (&mut inp.player2,&mut inp.player1)
+        let (winner,loser) = match does_player1_win(&mut inp,inp_is_recursive) {
+            Some(n) => match n {
+                       true =>  (&mut inp.player1,&mut inp.player2),
+                       false => (&mut inp.player2,&mut inp.player1)
+            },
+            None => return (0,true) //means player 1 won because we'd played a deck layout twice
         };
         (*winner).rotate_left(1);
         (*winner).push_back((*loser).pop_front().unwrap());
-        counter+=1;
     }
-    let final_winner = match inp.player1.len() == 0 {
-        true => &inp.player2,
-        false => &inp.player1
+    let (final_winner,did_player_1_win) = match inp.player1.len() == 0 {
+        true => (&inp.player2,false),
+        false => (&inp.player1,true),
     };
     let mut score = 0;
     let wlen = (*final_winner).len();
     for (idx_i,i) in (*final_winner).iter().enumerate() {
         score += (wlen-idx_i)* *i as usize;
     }
-    (score as Score,counter)
+    (score as Score,did_player_1_win)
+}
+
+fn does_player1_win(inp: &mut CardGame,inp_is_recursive:bool) -> Option<bool> {
+    //returns true or false, wrapped in an option - returns None if Player1 wins the game by
+    //repetitive deck
+    if !inp_is_recursive {
+        return Some(inp.player1.front().unwrap() > inp.player2.front().unwrap());
+    }
+    //playing by recursive rules... first look if we've played this deck
+    if we_have_played_this_deck(&inp) {  //player 1 wins if this is a repeat position
+        return None;
+    }
+    else { //add this deck layout to the list since it's new
+        let new_deck = CardGame {
+            player1: inp.player1.clone(),
+            player2: inp.player2.clone(),
+            history: Vec::new()
+        };
+        inp.history.push(new_deck);
+    }
+    let out;
+    // get values of the top cards
+    let size1 = *inp.player1.front().unwrap();
+    let size2 = *inp.player2.front().unwrap();
+    if (inp.player1.len() as Card) > size1 && (inp.player2.len() as Card) > size2 {
+        //we're gonna play a subgame
+        //create the deck
+        //for each deck... take the first card OFF the deck, then deal as many cards as that top
+        //card equalled
+        let mut new_deck = CardGame {
+            player1: inp.player1.iter().skip(1).take(size1 as usize).copied().collect::<VecDeque<Card>>(),
+            player2: inp.player2.iter().skip(1).take(size2 as usize).copied().collect::<VecDeque<Card>>(),
+            history: Vec::new()
+        };
+        //look for subgames that don't need to be played
+        //if player 1 holds a card greater than the sum of the two decks minus 1 (s)he will will
+        let max1 = *new_deck.player1.iter().max().unwrap(); 
+        if max1 > (size1 + size2 -1) && max1 > *new_deck.player2.iter().max().unwrap() {
+            return Some(true);
+        }
+        
+        
+        //play the subgame
+        let tmp = play_game(&mut new_deck,inp_is_recursive);
+        out=tmp.1;
+        //we only care who won
+    }
+    else {
+        out = inp.player1.front().unwrap() > inp.player2.front().unwrap();
+    }
+    Some(out)
 }
 
 
+
+fn we_have_played_this_deck(inp: &CardGame) -> bool {
+    //look for player1's deck and player2's deck showing up in the deck history for this game
+
+    'outer: for i in &inp.history {
+        if inp.player1.len() == i.player1.len() && 
+            inp.player2.len() == i.player2.len(){
+                for (j,k) in inp.player1.iter().zip(i.player1.iter()) {
+                    if j != k {
+                        continue 'outer;
+                    }
+                }
+                for (j,k) in inp.player2.iter().zip(i.player2.iter()) {
+                    if j != k {
+                        continue 'outer;
+                    }
+                }
+                return true;
+        }
+    }
+    false
+}
+            
 
